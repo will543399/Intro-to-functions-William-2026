38ea4958e0806b69d6615dfef0572ff4161d1e3d
will543399 <williamz111@nycstudents.net>
2026-09-17 08:49:53 -0400

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,20 +1,19 @@
-import turtle
-from turtle import *
-t=Turtle()
-t.shape("turtle")
-for i in range(3):
+#import turtle
+#from turtle import *
+#t=Turtle()
+#t.shape("turtle")
+#for i in range(3):
     print(i)
-for i in range(4):
-    t.fd(100)
-    t.lt(90)
-t.speed(0)
-    for i in range(100):
-        t.fd(100)
-        t.lt(90)
-        for i in range(4):
-            t.fd(100)
-            t.lt(90)
+#for i in range(4):
+#    t.fd(100)
+#    t.lt(90)
+#t.speed(0)
+#for i int.fd(100):
+#    t.lt(90)
+#    for i in range(4):
+#        t.fd(100)
+#        t.lt(90)
                 
-square(100)
+#square(100)
 
-
+print("you didnt sign out....")
